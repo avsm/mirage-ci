275df4be2bf73bc5f40d564347a1aeee667663ec
Anil Madhavapeddy <anil@recoil.org>
2016-11-03 13:06:48 +0000

    add a Docker_run module compatible with DataKitCI
    This may currently result in non reproducible builds, so care must
    be taken when using it.
    
    Signed-off-by: Anil Madhavapeddy <anil@recoil.org>

---
+++ src/docker_run.ml
@@ -0,0 +1,88 @@
+(*---------------------------------------------------------------------------
+   Copyright (c) 2016 Anil Madhavapeddy. All rights reserved.
+   Distributed under the ISC license, see terms at the end of the file.
+   %%NAME%% %%VERSION%%
+  ---------------------------------------------------------------------------*)
+
+open DataKitCI
+open! Astring
+open Lwt.Infix
+
+let src = Logs.Src.create "datakit-ci.docker-run" ~doc:"Docker plugin for DataKitCI"
+module Log = (val Logs.src_log src : Logs.LOG)
+
+let ( / ) = Datakit_path.Infix.( / )
+
+module Docker_run_key = struct
+  type t = string * string list
+  let compare = Pervasives.compare
+end
+
+module Docker_runner = struct
+  type t = {
+    label: string;
+    pool: Monitored_pool.t;
+    timeout: float;
+  }
+
+  module Key = Docker_run_key
+
+  type context = NoContext
+  type value = string
+
+  let title _t (img,cmd) =
+    Fmt.strf "docker run %s %s" img (String.concat ~sep:" " cmd)
+
+  let generate t ~switch ~log trans NoContext (img,cmd) =
+    let tee outputs s = List.iter (fun o -> o s) outputs in
+    Live_log.log log "Waiting for free pool slot to run";
+    Monitored_pool.use ~reason:"docker run" t.pool (fun () ->
+      Live_log.log log "Pool slot obtained, starting run";
+      Utils.with_timeout ~switch t.timeout (fun switch ->
+        Live_log.log log "docker run %s %s" img (String.concat ~sep:" " cmd);
+        let cmd = Array.of_list ("docker"::"run"::img::cmd) in
+        let cmd_output = Buffer.create 1024 in
+        Process.run ~switch ~output:(tee [Buffer.add_string cmd_output;Live_log.write log]) ("",cmd) >>= fun () -> 
+        Lwt.return (Buffer.contents cmd_output)
+      )
+    ) >>= fun cmd_output ->
+    let open Utils in
+    DK.Transaction.create_or_replace_file trans (Cache.Path.value / "output") (Cstruct.of_string cmd_output) >>*= fun () ->
+    Lwt.return (Ok cmd_output)
+
+  let branch _t (img,cmd) =
+    Fmt.strf "%s:%s" img (String.concat ~sep:" " cmd) |>
+    Digest.string |>
+    Digest.to_hex |>
+    Fmt.strf "docker-run-%s"
+
+  let load _t tr _key =
+    let open Utils in
+    DK.Tree.read_file tr (Datakit_path.of_string_exn "value/output") >>*= fun output ->
+    Lwt.return (Cstruct.to_string output)
+end
+ 
+module Docker_run_cache = Cache.Make(Docker_runner)
+
+type t = Docker_run_cache.t 
+let config ~logs ~label ~pool ~timeout =
+  Docker_run_cache.create ~logs { Docker_runner.label; pool; timeout }
+
+let run ~tag ~cmd config =
+  Docker_run_cache.term config Docker_runner.NoContext (tag,cmd)
+
+(*---------------------------------------------------------------------------
+   Copyright (c) 2016 Anil Madhavapeddy
+
+   Permission to use, copy, modify, and/or distribute this software for any
+   purpose with or without fee is hereby granted, provided that the above
+   copyright notice and this permission notice appear in all copies.
+
+   THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+   WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+   MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+   ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+   WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+   ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+   OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+  ---------------------------------------------------------------------------*)
---
+++ src/docker_run.mli
@@ -0,0 +1,37 @@
+(*---------------------------------------------------------------------------
+   Copyright (c) 2016 Anil Madhavapeddy. All rights reserved.
+   Distributed under the ISC license, see terms at the end of the file.
+   %%NAME%% %%VERSION%%
+  ---------------------------------------------------------------------------*)
+
+(** Run a Docker image and return the output *)
+
+open DataKitCI
+
+type t
+(** [t] is the state of a [Docker_run] instance *)
+
+val config : logs:Live_log.manager -> label:string -> pool:Monitored_pool.t -> timeout:float -> t
+(** [config ~logs ~label ~pool ~timeout] will configure a Docker runner to execute
+    images. [pool] controls the level of parallel builds allowed, and [timeout]
+    is the length in seconds that a run can execute for before being terminated. *)
+
+val run : tag:string -> cmd:string list -> t -> string Term.t
+(** [run t d] will run the [cmd] inside [tag] image using the [t] builder, and
+    return the string output of the command. *)
+
+(*---------------------------------------------------------------------------
+   Copyright (c) 2016 Anil Madhavapeddy
+
+   Permission to use, copy, modify, and/or distribute this software for any
+   purpose with or without fee is hereby granted, provided that the above
+   copyright notice and this permission notice appear in all copies.
+
+   THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+   WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+   MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+   ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+   WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+   ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+   OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+  ---------------------------------------------------------------------------*)
