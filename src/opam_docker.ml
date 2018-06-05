ec5867a0440314703785ece5f56013bf68a2214c
Kate <kit.ty.kate@disroot.org>
2018-06-05 15:07:44 +0100

    Remove temporary fixes for the docker images and opam-depext

--- src/opam_docker.ml
+++ src/opam_docker.ml
@@ -141,14 +141,9 @@ module V2 = struct
   let set_opam_repo_rev = V1.set_opam_repo_rev
 
   let base ~ocaml_version ~distro =
-    from ~tag:(distro^"-"^Oversions.docker_opam2 ocaml_version) "ocaml/opam2-staging" @@
+    from ~tag:(distro^"-"^Oversions.docker_opam2 ocaml_version) "ocaml/opam2" @@
     add_cache_dir @@
-    (* NOTE: Tmp fix while waiting for opam-depext > 1.1.1 *)
-    match distro with
-    | "opensuse-42.3" | "fedora-27" ->
-        run "opam pin add -yv opam-depext --dev"
-    | _ ->
-        run "opam install -yv opam-depext"
+    run "opam install -yv opam-depext"
 
   let add_archive_script =
     generate_sh "opam-ci-archive" [
